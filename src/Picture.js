b5060d0b09ed3167b59d47c948e3c0939781c4d6
ginatan <leesinshingbrian@gmail.com>
2017-10-09 17:54:23 +0800

    pushed url into picUrl array

--- src/Picture.js
+++ src/Picture.js
@@ -1,11 +1,21 @@
 import React, { Component } from 'react'
 
 export class Picture extends Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      picUrl: []
+    }
+    this.handleKeyUp = this.handleKeyUp.bind(this)
+  }
   // handling API CALL
   handleKeyUp (e) {
     // alert('hello')
     console.log(e.target.value)
     const url = 'https://api.giphy.com/v1/gifs/search?api_key=rRROY9dnpDWINQuHrcFXhjaa00nLqPPw&q=' + e.target.value + '&limit=8&offset=0&rating=G&lang=en'
+    this.setState({
+      picUrl: []
+    })
 
     fetch(url)
     .then((response) => {
@@ -14,8 +24,11 @@ export class Picture extends Component {
     })
     .then((data) => {
       // console.log(data)
-      var picture = (data.data[0].images.original.url)
-      console.log(picture)
+      var results = (data.data)
+      for (var i = 0; i < results.length; i++) {
+        this.state.picUrl.push(results[i].images.original)
+      }
+      console.log('picUrl', this.state.picUrl)
     })
   }
 
