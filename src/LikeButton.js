699e7a62bf333f053aa75a91516be247655abf9c
ginatan <leesinshingbrian@gmail.com>
2017-10-09 22:40:35 +0800

    button to change color upon click

--- src/LikeButton.js
+++ src/LikeButton.js
@@ -4,19 +4,28 @@ export class LikeButton extends Component {
   constructor (props) {
     super(props)
     this.state = {
-      isMouseInside: props.isMouseInside,
-      isItLike: false
+      display: 'none',
+      color: 'white'
     }
+    this.handleClick = this.handleClick.bind(this)
+  }
+  // - upon click, button to change color and remain there
+  handleClick () {
+    const newColor = this.state.color === 'white' ? 'red' : 'white'
+    const newDisplay = this.state.display === 'none' ? 'inline' : 'none'
+    this.setState({
+      display: newDisplay,
+      color: newColor
+    })
+    console.log(this.state.color)
+    console.log(this.state.display)
   }
 
   render () {
-    if (this.props.isMouseInside) {
-      return (
-        <div>
-          <button>like</button>
-          {/* {this.state.isMouseInside ? <button>like</button> : null} */}
-        </div>
-      )
-    } else return null
+    return (
+      <div>
+        <button onClick={this.handleClick} style={{background: this.state.color}}>like</button>
+      </div>
+    )
   }
 }
--- src/Picture.js
+++ src/Picture.js
@@ -5,12 +5,9 @@ export class Picture extends Component {
   constructor (props) {
     super(props)
     this.state = {
-      picUrl: [],
-      isMouseInside: false
+      picUrl: []
     }
     this.handleKeyUp = this.handleKeyUp.bind(this)
-    this.mouseEnter = this.mouseEnter.bind(this)
-    this.mouseLeave = this.mouseLeave.bind(this)
   }
 
   // - handling API CALL
@@ -45,23 +42,12 @@ export class Picture extends Component {
     })
   }
 
-  // - handling hovering
-  mouseEnter () {
-    this.setState({ isMouseInside: true })
-    // console.log(this.state.isMouseInside)
-  }
-
-  mouseLeave () {
-    this.setState({ isMouseInside: false })
-    // console.log(this.state.isMouseInside)
-  }
-
   render () {
     // - for display of pictures
     let displayPic = this.state.picUrl.map((url, index) => {
       return (
-        <div className='picture' key={index} onMouseEnter={this.mouseEnter} onMouseLeave={this.mouseLeave} style={{backgroundImage: `url(${url})`}}>
-          <LikeButton key={index} isMouseInside={this.state.isMouseInside} />
+        <div className='picture' key={index} style={{backgroundImage: `url(${url})`}}>
+          <LikeButton />
         </div>
       )
     })
